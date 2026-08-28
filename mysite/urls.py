2eddccfb5feefc9f56543e61f561ae198ac9c43c
Елена Чечельницкая <petrakova9@mail.ru>
2026-08-28 06:53:20 +0300

    Тема 7: Swagger, CORS, Docker и docker-compose с PostgreSQL, порядок запуска

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,22 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from employees.views import index
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Employees API",
+        default_version="v1",
+        description="API для управления сотрудниками",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,6 +24,21 @@ urlpatterns = [
     path("employees/", include("employees.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("api/", include("employees.api_urls")),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
 
 # Подключение медиафайлов только в режиме разработки
